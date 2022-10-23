676554ccb3a0ff95ffcf80e8af3c8392ddee1e4c
ajaysharma12799 <ajaysharma12799@gmail.com>
2022-10-23 12:05:57 +0530

    added Major CRUD Ops with sort, filter, limit

--- controllers/tours.controller.js
+++ controllers/tours.controller.js
@@ -1,53 +1,142 @@
-const fs = require('fs');
-const tours = require('../helper/data.json');
+const TourModel = require('../models/tours.models');
 
-const getAllTours = (req, res) => {
-    res.status(200).json({ 
-        status: 'success',
-        result: tours.length,
-        tours
-     });
-}
+const getAllTours = async (req, res) => {
+    // Basic Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'filter', 'sort', 'limit'];
+    excludedFields.forEach(el => delete queryObj[el]);
 
-const getTour = (req, res) => {
-    const id = parseInt(req.params.id);
-    const tour = tours.find(tour => tour.id === id);
+    // Advance Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
-    res.status(200).json({ 
-        status: 'success',
-        tour,
-     });
-}
+    try {
+        let query = TourModel.find(JSON.parse(queryStr));
 
-const addTour = (req, res) => {
-    const newID = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newID }, req.body);
+        // Sorting
+        if(req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        }
+        else {
+            query = query.sort('-createdAt');
+        }
 
-    fs.writeFile(fs.readFileSync(`${__dirname}../helper/data.json`), JSON.stringify(newTour), (error) => {
-        if(error) console.log(error.message);
+        // Field Limiting
+        if(req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields);
+        }
+        else {
+            query = query.select('-__v');
+        }
+        /**
+         * Selecting or Removing Fields with select Method is known as Projection
+        */
 
-        res.status(200).json({
+        // Pagination
+        const page = req.query.page * 1 || 1;
+        const limit = req.query.limit * 1 || 100;
+        const skip = (page - 1) * limit;
+
+        query = query.skip(skip).limit(limit);
+
+        if(req.query.page) {
+            const numOfPage = await TourModel.countDocuments();
+            if(skip >= numOfPage) throw new Error("This Page Does't Exist");
+        }
+
+        const tours = await query;
+        res.status(200).json({ 
             status: 'success',
             result: tours.length,
             data: {
-                newTour
+                tours
+            }
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 'failed',
+            message: error.message
+        })
+    }
+}
+
+const getTour = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const tour = await TourModel.findById(id);
+
+        res.status(200).json({ 
+            status: 'success',
+            data: {
+                tour,
+            }
+        });
+    } catch (error) {
+        res.status(404).json({
+            status: 'failed',
+            message: error.message
+        })
+    }
+}
+
+const addTour = async (req, res) => {
+    try {
+        const newTour = await TourModel.create(req.body);
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour: newTour,
             }
         })
-    })
+    } catch (error) {
+        res.status(400).json({
+            status: 'failed',
+            message: error.message
+        });
+    }
 }
 
-const updateTour = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        msg: "Update Request"
-    })
+const updateTour = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const tour = await TourModel.findByIdAndUpdate(id, req.body, {
+            new: true, // Always Return Updated Document
+            runValidators: true, // Run Validator on New Value Again
+        })
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        })   
+    } catch (error) {
+        res.status(400).json({
+            status: 'failed',
+            message: error.message
+        });
+    }
 }
 
-const deleteTour = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        msg: "Delete Request"
-    })
+const deleteTour = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const tour = await TourModel.findByIdAndDelete(id)
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        })   
+    } catch (error) {
+        res.status(400).json({
+            status: 'failed',
+            message: error.message
+        });
+    }
 }
 
 module.exports = { getAllTours, getTour, addTour, updateTour, deleteTour };
